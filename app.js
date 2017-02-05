76e68acb8ce2db12e17be77b2e1186f9ac8e714e
Simone Celia <Simone Celia>
2017-02-05 20:46:28 +0100

    prova template grafico

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const url = require('url');
 const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow;
+
 app.on('ready', function(){
-	mainWindow = new BrowserWindow({width: 1024, height: 768, /*backgroundColor: '#2e2c29'*/});
+	mainWindow = new BrowserWindow({/*width: 400, height: 300,*/ /*backgroundColor: '#2e2c29'*/});
 	//mainWindow.loadURL('https://github.com');
 	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'main_view.html'),
+		pathname : path.join(__dirname, 'index.html'),
+		//pathname: path.join(__dirname, 'html', 'main.html'),
 		protocol: 'file',
 		slashes: true
 	}));
+	
+	
 });
 
