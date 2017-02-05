a52245c5fe0449a5884ecfc9d6768d8551829109
Simone Celia <Simone Celia>
2017-02-06 00:59:52 +0100

    fix grafica

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const BrowserWindow = electron.BrowserWindow;
 var mainWindow;
 
 app.on('ready', function(){
-	mainWindow = new BrowserWindow({/*width: 400, height: 300,*/ /*backgroundColor: '#2e2c29'*/});
+	mainWindow = new BrowserWindow();//"/*width: 400, height: 300,*/ /*backgroundColor: '#2e2c29'*/});
 	//mainWindow.loadURL('https://github.com');
+
 	mainWindow.loadURL(url.format({
 		pathname : path.join(__dirname, 'index.html'),
 		//pathname: path.join(__dirname, 'html', 'main.html'),
