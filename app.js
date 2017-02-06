00d64ff5a29849b7f2fc7d69faa1936f06a0c91b
simone <scelia@laptop-job>
2017-02-06 19:10:25 +0100

    test scelta db

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow;
 
-app.on('ready', function(){
-	mainWindow = new BrowserWindow();//"/*width: 400, height: 300,*/ /*backgroundColor: '#2e2c29'*/});
+//const nw = require('nw.gui');
+
+app.on('ready', function() {
+	mainWindow = new BrowserWindow(); //"/*width: 400, height: 300,*/ /*backgroundColor: '#2e2c29'*/});
 	//mainWindow.loadURL('https://github.com');
 
 	mainWindow.loadURL(url.format({
-		pathname : path.join(__dirname, 'index.html'),
+		pathname: path.join(__dirname, 'index.html'),
 		//pathname: path.join(__dirname, 'html', 'main.html'),
 		protocol: 'file',
 		slashes: true
 	}));
-	
-	
-});
 
+	// Type 4: Persistent datastore for a Node Webkit app called 'nwtest'
+	// For example on Linux, the datafile will be ~/.config/nwtest/nedb-data/something.db
+	//var Datastore = require('nedb'),
+	//path = require('path'),
+	//db = new Datastore({ filename: path.join(nw.App.dataPath, 'something.db') });
+});
