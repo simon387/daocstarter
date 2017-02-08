493dee842b5b61729fc50c81e7284529dca52aa1
Simone Celia <Simone Celia>
2017-02-08 01:11:39 +0100

    embedded db trovato: tingodb, è nosql

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ var mainWindow;
 //const nw = require('nw.gui');
 
 app.on('ready', function() {
+
+	let Db = require('tingodb')().Db;
+	let assert = require('assert');
+
+	var db = new Db('c:\\db', {});
+	// Fetch a collection to insert document into
+	var collection = db.collection("batch_document_insert_collection_safe");
+	let col = db.collection("tabella");
+	col.insert([{numero:2}], {w:1}, function(err, result) {});
+
+	// Insert a single document
+	collection.insert([{hello:'world_safe1'}
+	, {hello:'world_safe2'}], {w:1}, function(err, result) {
+		assert.equal(null, err);
+
+		// Fetch the document
+		collection.findOne({hello:'world_safe2'}, function(err, item) {
+			assert.equal(null, err);
+			assert.equal('world_safe2', item.hello);
+		})
+	});
+
+
+
+
+
 	mainWindow = new BrowserWindow(); //"/*width: 400, height: 300,*/ /*backgroundColor: '#2e2c29'*/});
 	//mainWindow.loadURL('https://github.com');
 
