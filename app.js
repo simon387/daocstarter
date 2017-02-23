ec8a34476b3fb7df579b4d76608b94f743c43baa
Simone Celia <Simone Celia>
2017-02-23 01:29:20 +0100

    odio js, è tutto async

--- app.js
+++ app.js
@@ -16,14 +16,25 @@ let accounts = db.collection('account');
 //creazione di una riga di test
 //accounts.insert([{name:'nome',email:'email',mobile:'mobile',start_date:'start_date'}], {w:1}, function(err, result) {});
 
-let cursor = accounts.find();
+let cursor = accounts.find({});
+let stringa = '{"aaData":[';
+
 cursor.each(function(err, item) {
 	// If the item is null then the cursor is exhausted/empty and closed
 	if(item == null) {
+		stringa = stringa.slice(0, -1);
+		stringa += ']}';
+		console.log(stringa);
+
+
+
+
+	} else {
+		//console.log(stringa);
+		stringa += + '["' + 1 + '","' + item.name + '","' + item.email + '","' + item.mobile + '","' + item.start_date + '"],';
 	}
-	// otherwise, do something with the item
-	console.log(item);
 });
+
 
 //
 // creazione server express
@@ -50,7 +61,7 @@ server.get('/', function (req, res) {
 	//risposta fake per i test
 	res.setHeader('Content-Type', 'application/json');
 	res.send(JSON.stringify( {"aaData":[["7", "1", "1@1.1", "1", null, "<a data-id=\"row-7\" href=\"javascript:editRow(7);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(7);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["21", "1", "1@1.1", "1", null, "<a data-id=\"row-21\" href=\"javascript:editRow(21);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(21);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["6", "marco", "darinasd@pollo.com", "asd", null, "<a data-id=\"row-6\" href=\"javascript:editRow(6);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(6);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["5", "simone", "45687@hotmail.it", "345", null, "<a data-id=\"row-5\" href=\"javascript:editRow(5);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(5);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["18", "h", "asfd@gasofj.colm", "h", null, "<a data-id=\"row-18\" href=\"javascript:editRow(18);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(18);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["19", "1", "1@1.1", "1", null, "<a data-id=\"row-19\" href=\"javascript:editRow(19);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(19);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["20", "1", "1@1.1", "1", null, "<a data-id=\"row-20\" href=\"javascript:editRow(20);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(20);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["14", "a", "a@a.a", "a", null, "<a data-id=\"row-14\" href=\"javascript:editRow(14);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(14);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["15", "a", "a@a.a", "a", null, "<a data-id=\"row-15\" href=\"javascript:editRow(15);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(15);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["16", "a", "a@a.a", "a", null, "<a data-id=\"row-16\" href=\"javascript:editRow(16);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(16);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"],["17", "prova", "chiocciola@c.o", "asd", null, "<a data-id=\"row-17\" href=\"javascript:editRow(17);\" class=\"btn btn-md btn-success\">edit<\/a>&nbsp;<a href=\"javascript:removeRow(17);\" class=\"btn btn-default btn-md\" style=\"background-color: #c83a2a;border-color: #b33426; color: #ffffff;\">remove<\/a>"]]}));
-	//res.send(JSON.stringify(accounts.find()));
+	//res.send(JSON.stringify(stringa));
 
 });
 
