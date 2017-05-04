07c9d366b6087e7aaa6bc64eea2f6fd7af770616
Simone Celia <simon387@hotmail.it>
2017-05-04 19:47:13 +0200

    tentativo di fix segnalazione senza log dropdown res

--- js/vga-module.js
+++ js/vga-module.js
@@ -6,44 +6,50 @@ const db = require('./db-module.js');
 
 module.exports = {
 	getAllResolutions: response => {
-		if (os.platform() != 'win32') {
-			return response.send(["4096×2160"]);
-		}
-		let ps = new shell({executionPolicy: 'Bypass', debugMsg: false, noProfile: true});
-		ps.addCommand('get-wmiobject -query "' +
-		'select HorizontalResolution, VerticalResolution ' +
-		'from CIM_VideoControllerResolution" | ' +
-		'Sort-Object HorizontalResolution, VerticalResolution -descending | ' +
-		'select HorizontalResolution, VerticalResolution')
-		.then(() => {
-			return ps.invoke();
-		})
-		.then(output => {
-			let str = output.replace(/[\n\r]/g, '').replace(/\ +/g, 'x');
-			let regexp = /\d+x+\d+/g;
-			let match;
-			let matches = new Set();
-			while ((match = regexp.exec(str)) != null) {
-				matches.add(match[0]);
-			}
-			ps.dispose();
-			if (0 == matches.length) {
-				db.settingDatastore.findOne({key: 'custom.resolutions.comma.separated'}, (err, doc) => {
-					const array = doc.value.split(',');
-					if (array.length != 0) {
-						response.send(array)
+		
+		db.settingDatastore.findOne({key: 'custom.resolutions.comma.separated'}, (err, setting) => {
+			const array = setting.value.split(',');
+			let ps;
+			try{
+				ps = new shell({executionPolicy: 'Bypass', debugMsg: false, noProfile: true});
+					ps.addCommand('get-wmiobject -query "' +
+				'select HorizontalResolution, VerticalResolution ' +
+				'from CIM_VideoControllerResolution" | ' +
+				'Sort-Object HorizontalResolution, VerticalResolution -descending | ' +
+				'select HorizontalResolution, VerticalResolution')
+				.then(() => {
+					return ps.invoke();
+				})
+				.then(output => {
+					let str = output.replace(/[\n\r]/g, '').replace(/\ +/g, 'x');
+					let regexp = /\d+x+\d+/g;
+					let match;
+					let matches = new Set();
+					while ((match = regexp.exec(str)) != null) {
+						matches.add(match[0]);
+					}
+					ps.dispose();
+					if (0 == matches.length) {
+						if (array.length != 0) {
+							response.send(array);
+						}
+						else {
+							response.send(['1920x1200', '800x600']);
+						}
 					}
 					else {
-						response.send(['1920x1200', '800x600']);
+						response.send(Array.from(matches));
 					}
+				})
+				.catch(err => {
+					ps.dispose();
+					response.send(array);
 				});
 			}
-			else {
-				response.send(Array.from(matches));
+			catch(e) {
+				console.log(e);
+				response.send(array);
 			}
-		})
-		.catch(err => {
-			ps.dispose();
 		});
 	}
 };
