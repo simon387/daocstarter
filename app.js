6581758d5236a49d52a12fa9ed18b22fd8e15ffe
Simone Celia <Simone Celia>
2017-05-31 22:48:13 +0200

    https://github.com/simon387/daocstarter/issues/4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const electron = require('electron');
+const {app, Menu, Tray, BrowserWindow} = require('electron');
 const packageJSON = require('./package.json');
 const path = require('path');
 const commonUtil = require('./js/controller/common-util.js');
@@ -10,8 +10,28 @@ require('./js/ipc-module');
 require('./js/express-module.js');
 require('./js/menu-module.js');
 
-electron.app.on('ready', () => {
-	const BrowserWindow = electron.BrowserWindow;
+let tray = null;
+
+app.on('ready', () => {
+	tray = new Tray('img/i.ico')
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Restore Daocstarter',
+			click: () => {
+				mainWindow.show();
+			}
+		},
+		{
+			label: 'Quit',
+			click: () => {
+				app.isQuiting = true;
+				app.quit();
+			}
+		}
+	]);
+	tray.setToolTip('Daocstarter!');
+	tray.setContextMenu(contextMenu);
+
 	const mainWindow = new BrowserWindow({
 		width: 1400,
 		height: 809,
@@ -22,18 +42,42 @@ electron.app.on('ready', () => {
 		title: 'DAoC Starter v' + packageJSON.version,
 		icon: 'img/i.ico'
 	});
+
 	mainWindow.loadURL(require('url').format({
 		pathname: path.join(__dirname, 'html', 'views', 'main.html'),
 		protocol: 'file',
 		slashes: true
 	}));
+
 	mainWindow.once('ready-to-show', () => {
 		mainWindow.show();
 	});
-	//mainWindow.webContents.openDevTools();//dev mode automatica
+
 	mainWindow.on('resize', () => {
 		commonUtil.moveFavourites(mainWindow.getSize());
 	});
+
+	mainWindow.on('minimize', event => {
+		event.preventDefault()
+		mainWindow.hide();
+	});
+
+	mainWindow.on('close', event => {
+		if(!app.isQuiting) {
+			event.preventDefault()
+			mainWindow.hide();
+		}
+		return false;
+	});
+
+	tray.on('click', event => {
+		if (mainWindow.isVisible()) {
+			mainWindow.hide();
+		}
+		else {
+			mainWindow.show();
+		}
+	});
 });
 
-electron.app.on('window-all-closed', electron.app.quit);//altrimenti al quit lascia i processi appesi-.-
+app.on('window-all-closed', app.quit);//altrimenti al quit lascia i processi appesi-.-
